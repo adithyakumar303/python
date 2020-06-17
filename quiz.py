16b1b5a239e5dee2a2e9e5321d6efeb0901adf4d
adithyakumar303 <65886066+adithyakumar303@users.noreply.github.com>
2020-06-17 19:18:24 +0530

    my first python program

---
+++ quiz.py
@@ -0,0 +1,156 @@
+print("genaral knolsdge quiz")
+score=0
+print("\n")
+print("1. who is the first man on the moon")
+answer=input("enter you answer: ") 
+if answer == "neil armstrong":
+   
+   print("answer is correct")
+   score=score+1
+else:
+	print("anser is wrong")
+print("\n")
+print("2. who is the first man to climb mount everest")
+answer=input("enter you answer: ") 
+if answer == "edmund hillary":
+   
+   print("answer is correct")
+   score=score+1
+else:
+	print("anser is wrong")
+
+print("\n")
+print("3. who was the 16 president of the united states")
+answer=input("enter you answer: ") 
+if answer == "abraham lincon":
+   
+   print("answer is correct")
+   score=score+1
+else:
+	print("anser is wrong")
+	
+	print("\n")
+print("4. who is the current president of india")
+answer=input("enter you answer: ") 
+if answer == "ram nath kovind":
+   
+   print("answer is correct")
+   score=score+1
+else:
+	print("anser is wrong")
+print ("score= ",score)
+
+
+
+
+
+print("\n")
+print("5. who discovored penicillin")
+answer=input("enter you answer: ") 
+if answer == "alexander fleming":
+   
+   print("answer is correct")
+   score=score+1
+else:
+	print("anser is wrong")
+	
+	
+
+
+
+
+
+print("\n")
+print("6. who created stan lee")
+answer=input("enter you answer: ") 
+if answer == "stan lee":
+   
+   print("answer is correct")
+   score=score+1
+else:
+	print("anser is wrong")
+
+
+
+ 
+print("\n")
+print("6. who was the first president of USA")
+answer=input("enter you answer: ") 
+if answer == "GEORGE washinton":
+   
+   print("answer is correct")
+   score=score+1
+else:
+	print("anser is wrong")
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("\n")
+print("8. who invented telephone")
+answer=input("enter you answer: ") 
+if answer == "alexsander grahambel":
+   
+   print("answer is correct")
+   score=score+1
+else:
+	print("answer is wrong")
+
+
+
+print("\n")
+print("9. who is th the co-founder of microsoft")
+answer=input("enter you answer: ") 
+if answer == "bill gates":
+   
+   print("answer is correct")
+   score=score+1
+else:
+	print("answer is wrong")
+	
+	
+	
+	print("\n")
+print("9. who founded apple ")
+answer=input("enter you answer: ") 
+if answer == "steve jobs":
+   
+   print("answer is correct")
+   score=score+1
+else:
+	print("answer is wrong")
+	
+print ("score= ",score)
